c59cbf0285b16a8bbcd0f079b9a321795729363b
hum-dev <humphreymuriungi@outlook.com>
2023-05-23 12:37:02 +0300

    Timer

--- src/logic.jsx
+++ src/logic.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
 const CountdownTimer = () => {
   // State variables to manage countdown time, remaining time, and countdown state
